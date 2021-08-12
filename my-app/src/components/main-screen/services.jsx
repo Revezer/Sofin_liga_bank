74d4d71c4465162d89fc8ac354be713e5ae995b1
Revezer <64742445+Revezer@users.noreply.github.com>
2021-08-12 18:24:19 +0300

    services-tablet

--- my-app/src/components/main-screen/services.jsx
+++ my-app/src/components/main-screen/services.jsx
@@ -63,14 +63,14 @@ const Services = () => {
                     <button className='service__button'>Узнать подробнее</button>
                 </SwiperSlide>
                 <SwiperSlide className='service__container service__conteiner-telephone'>
-                    <h3 className='service__title'>
+                    <h3 className='service__title service__title--nowrap'>
                         {'Лига Банк — это огромное\nколичество онлайн-сервисов\nдля вашего удобства'}
                     </h3>
                     <div className='service__text-conteiner service__text-contener--fourth'>
                         <span className='service__text'>
                             {'Мобильный банк,\nкоторый всегда под рукой'}
                         </span>
-                        <span className='service__text'>
+                        <span className='service__text service__text--width'>
                             Приложение Лига-проездной
                             позволит вам оплачивать
                             билеты по всему миру
